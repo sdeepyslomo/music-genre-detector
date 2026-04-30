ff3722ec4b94c93fef505f81b08f24a28afaab07
Soumyadeep-551 <soumyad2005@gmail.com>
2026-05-01 00:30:20 +0530

    Add MFCC feature extraction

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
-import requests
+import librosa
 
-#download a web page
-response = requests.get("https://api.github.com")
-print(response.status_code)
+file_path = "data/bleed.wav"
+
+audio, sample_rate = librosa.load(file_path)
+
+mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=13)
+
+print("Audio loaded successfully!")
+print("Sample Rate:", sample_rate)
+print("Audio Length (samples):", len(audio))
+print("MFCC shape:", mfccs.shape)
