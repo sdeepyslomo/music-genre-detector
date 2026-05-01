c8a504f61640468878e415b3524d0afcd4ceaa89
Soumyadeep-551 <soumyad2005@gmail.com>
2026-05-02 03:26:35 +0530

    Added GTZAN dataset pipeline with scaling

--- src/main.py
+++ src/main.py
@@ -5,8 +5,14 @@ from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.svm import SVC
+import kagglehub
+from sklearn.preprocessing import StandardScaler
 
-DATASET_PATH = "data"
+DATASET_PATH = kagglehub.dataset_download("andradaolteanu/gtzan-dataset-music-genre-classification")
+print("Path to dataset files:", DATASET_PATH)
+print("Inside:", os.listdir(DATASET_PATH))
+
+DATASET_PATH = os.path.join(DATASET_PATH, "Data", "genres_original")
 
 features = []
 labels = []
@@ -44,13 +50,16 @@ print("x shape:", x.shape)
 print("y shape:", y.shape)
 
 x_train, x_test, y_train, y_test = train_test_split(
-    x,y,test_size=0.5, random_state=42
+    x,y,test_size=0.2, random_state=42
 )
-
 print("Training samples:", len(x_train))
 print("Testing samples:", len(x_test))
 
-knn = KNeighborsClassifier(n_neighbors=1)
+scaler = StandardScaler()
+x_train = scaler.fit_transform(x_train)
+x_test = scaler.transform(x_test)
+
+knn = KNeighborsClassifier(n_neighbors=7)
 knn.fit(x_train,y_train)
 print("KNN Accuracy:", accuracy_score(y_test, knn.predict(x_test)))
 
@@ -62,11 +71,10 @@ file_path = "data/silvera.wav"
 audio, sample_rate = librosa.load(file_path)
 mfccs = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=13)
 mfccs_mean = np.mean(mfccs,axis=1)
-mfccs_mean = mfccs_mean.reshape(1,-1)
 mfccs_std = np.std(mfccs,axis=1)
-mfccs_std = mfccs_std.reshape(1,-1)
 conc = np.concatenate((mfccs_mean,mfccs_std))
 conc = conc.reshape(1,-1)
+conc = scaler.transform(conc)
 prediction = svc.predict(conc)
 print("Predicted genre:", prediction[0])
 
